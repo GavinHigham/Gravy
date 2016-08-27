1bdf35d77021cd756239d742437a141bf7ffbac7
GavinHigham <Gavin.Higham@gmail.com>
2016-08-27 16:36:52 -0700

    Fix deprecated function call

--- main.lua
+++ main.lua
@@ -5,16 +5,29 @@ planets = {}
 planet_positions = {}
 pressed = {x=0, y=0}
 
+-- function findGravity(affected, affectee)
+-- 	local x1, y1 = affected:getPosition()
+-- 	local x2, y2 = affectee:getPosition()
+-- 	local dx = x1-x2
+-- 	local dy = y1-y2
+-- 	local r2 = dx*dx + dy*dy
+-- 	local m1 = affected:getMass()
+-- 	local m2 = affectee:getMass()
+-- 	local force = -gConst*m1*m2*1000/r2
+-- 	return force*dx/r2, force*dy/r2
+-- end
+
 function findGravity(affected, affectee)
 	local x1, y1 = affected:getPosition()
 	local x2, y2 = affectee:getPosition()
 	local dx = x1-x2
 	local dy = y1-y2
 	local r2 = dx*dx + dy*dy
+	local r = math.sqrt(r2)
 	local m1 = affected:getMass()
-	local m2 = affectee:getMass()
-	local force = -gConst*m1*m2/r2
-	return force*dx/r2, force*dy/r2
+	local m2 = affected:getMass()
+	local force = -gConst*m1*m2*1000/r2
+	return (dx/r)*force, (dy/r)*force
 end
 
 --function newPlanet(posX, posY, )
@@ -36,12 +49,12 @@ function love.load()
 	-- Create some planets!
 	for i=1,8 do
 		for j=1,6 do
-			if love.math.noise(i+8, j+60) > 0.8 then
+			if love.math.noise(i+8, j+30) > 0.8 then
 				local posX = 100*i-50
 				local posY = 100*j-50
 				local body = love.physics.newBody(world, posX, posY, 'dynamic')
 				local shape = love.physics.newCircleShape(30)
-				local density = 20000
+				local density = 20
 				local fixture = love.physics.newFixture(body, shape, density)
 				table.insert(planets, body)
 			end
@@ -61,14 +74,28 @@ function love.load()
 			dir.y = 0;
 			for(int i = 0; i < ]].. numPlanets ..[[; i++) {
 				vec2 dist = (planets[i]-screen_coords);
-				dir += dist/(dist.x*dist.x+dist.y*dist.y);
+				float mag = sqrt(dist.x*dist.x + dist.y*dist.y);
+				dir += (dist/(mag*mag*mag));
 			}
-			dir = (dir * 30);
-			vec2 neutral = vec2(pow(1-abs(dir.x), 5), pow(1-abs(dir.y), 5))*neutral;
-			//float red = (neutral.x + neutral.y)/2;
-			float red = 1 - sqrt(pow(dir.x, 2) + pow(dir.y, 2))/sqrt(2.0);
-			dir = (dir + 1)/2;
-			return vec4(vec3(red, dir.x, dir.y)*channels, 1.0);
+			//dir /= (]] .. numPlanets .. [[);
+			float scale = 100;
+			//dir = (dir*scale+1)/2; //Bring dir into the range of [0,1].
+			//float power = 5;
+			//dir.x = pow(dir.x, power);
+			//dir.y = pow(dir.y, power);
+			dir = 10000*dir;
+			dir.x = atan(dir.x);
+			dir.y = atan(dir.y);
+			vec2 neutrality = vec2(pow(1-abs(dir.x), 5), pow(1-abs(dir.y), 5))*neutral;
+			float red = (neutrality.x + neutrality.y)/2; //This looks really cool.
+			//float red = 1 - sqrt(pow(dir.x, 2)*neutral.x + pow(dir.y, 2)*neutral.y)/sqrt(2.0);
+			//float red = atan(dir.y/dir.x);
+			//red = (sin(50*red)+1)/2;
+			//dir = (dir + 1)/2;
+			vec3 rgb = vec3(red, dir.x, dir.y)*channels;
+			vec2 neut = neutral; //Just to get rid of the compiler error.
+			float alpha = max(rgb.r, max(rgb.g, rgb.b));
+			return vec4(rgb, alpha);
 		}
 	]]
 	shader = love.graphics.newShader(fragment)
@@ -102,7 +129,7 @@ function applyGravity()
 		for j,planet in pairs(planets) do
 			local fx, fy = findGravity(missile, planet)
 			missile:applyLinearImpulse(fx, fy)
-			planet:applyLinearImpulse(-fx, -fy)
+			--planet:applyLinearImpulse(-fx, -fy)
 		end
 	end
 end
@@ -114,6 +141,7 @@ function love.update(dt)
 end
 
 function love.draw()
+	love.graphics.draw(starImage, 0, 0, 0, 1, 1, 0, 0, 0, 0)
 	local channels = {0, 0, 0}
 	local neutral = {1, 1}
 	if love.keyboard.isDown("a") then channels[1] = 1 end
@@ -136,7 +164,7 @@ function love.draw()
 		local x, y = missile:getPosition()
 		love.graphics.circle("fill", x, y, 10, 36)
 	end
-	if love.mouse.isDown("l") then
+	if love.mouse.isDown(1) then
 		local mx, my = love.mouse.getPosition()
 		love.graphics.line(pressed.x, pressed.y, mx, my)
 	end
